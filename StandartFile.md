4f8ee8a4986d80a301f488277be2920671f365b6
Superior <geizer@ro.ru>
2020-03-26 21:52:45 +0400

    Init

--- StandartFile.md
+++
@@ -1,5 +0,0 @@
-# app-on-react
-simple messager, first stage (chekpoint)
-
-This app was created attempt programing,   noooooo
-This app was created attempt programing  suck if he update text, I`m killing he
---
+++ precache-manifest.2694e297a24a6a32402d1a2795158f17.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "030c7ebf94101faa77929cc8c7c27c0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "404a2be275faa7bbd05d",
+    "url": "/static/css/main.4df3c33d.chunk.css"
+  },
+  {
+    "revision": "2c7f3d614716f7b6e070",
+    "url": "/static/js/2.46cb8fed.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/static/js/2.46cb8fed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50d563f5f47db25a14bb",
+    "url": "/static/js/3.9a944b68.chunk.js"
+  },
+  {
+    "revision": "fb7aea5b5e1478fed2a7",
+    "url": "/static/js/4.a1b35e16.chunk.js"
+  },
+  {
+    "revision": "04f75e85f6d75d013d32",
+    "url": "/static/js/5.7459c355.chunk.js"
+  },
+  {
+    "revision": "404a2be275faa7bbd05d",
+    "url": "/static/js/main.423b186d.chunk.js"
+  },
+  {
+    "revision": "e6d7023542098608172f",
+    "url": "/static/js/runtime-main.34e94a90.js"
+  },
+  {
+    "revision": "1470696437779b51db2e97340112e88d",
+    "url": "/static/media/829.14706964.svg"
+  },
+  {
+    "revision": "a6288c49d2b59076e8b29c78cce6cba8",
+    "url": "/static/media/repit.a6288c49.png"
+  }
+]);
