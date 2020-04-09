87dda63a5f2d20587f0e1845cfa854f3cbcc84fd
Superior <geizer@ro.ru>
2020-04-09 19:09:21 +0400

    'fixMass

--- precache-manifest.0d90f3184a9e5f94cc00da2aca858033.js
+++ precache-manifest.0d90f3184a9e5f94cc00da2aca858033.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c31ed4b1f35c53db180e4bf57b9ac9a1",
+    "revision": "cc8a2fcfe625a5d3a17a961129b8b2da",
     "url": "/app-on-react/index.html"
   },
   {
-    "revision": "1d972cde6f7779a6cb1e",
-    "url": "/app-on-react/static/css/3.d0147d8a.chunk.css"
+    "revision": "ad44cb27731529d5035c",
+    "url": "/app-on-react/static/css/3.dc28a514.chunk.css"
   },
   {
     "revision": "1a07a73a7d3107d312c2",
     "url": "/app-on-react/static/css/4.b560ab40.chunk.css"
   },
   {
-    "revision": "9f145fba0a679e536b25",
+    "revision": "7ea364b0093a2698fb8d",
     "url": "/app-on-react/static/css/5.fe1cd19b.chunk.css"
   },
   {
-    "revision": "815686009434533060c1",
-    "url": "/app-on-react/static/css/main.8b25a7de.chunk.css"
+    "revision": "3e0f3c7db9a910942187",
+    "url": "/app-on-react/static/css/main.d58108c0.chunk.css"
   },
   {
     "revision": "5caa7c64238937578206",
@@ -28,24 +28,24 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/app-on-react/static/js/2.3ea55411.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1d972cde6f7779a6cb1e",
-    "url": "/app-on-react/static/js/3.37e233bb.chunk.js"
+    "revision": "ad44cb27731529d5035c",
+    "url": "/app-on-react/static/js/3.df10ac0a.chunk.js"
   },
   {
     "revision": "1a07a73a7d3107d312c2",
     "url": "/app-on-react/static/js/4.bb9e1ead.chunk.js"
   },
   {
-    "revision": "9f145fba0a679e536b25",
-    "url": "/app-on-react/static/js/5.dc8209a6.chunk.js"
+    "revision": "7ea364b0093a2698fb8d",
+    "url": "/app-on-react/static/js/5.ecb8e3c9.chunk.js"
   },
   {
-    "revision": "815686009434533060c1",
-    "url": "/app-on-react/static/js/main.57f89b71.chunk.js"
+    "revision": "3e0f3c7db9a910942187",
+    "url": "/app-on-react/static/js/main.3728d4ee.chunk.js"
   },
   {
-    "revision": "aa68d7be30bc05a25548",
-    "url": "/app-on-react/static/js/runtime-main.3dfb4e4a.js"
+    "revision": "5dc987f728e89b8a1a23",
+    "url": "/app-on-react/static/js/runtime-main.69b507f2.js"
   },
   {
     "revision": "1470696437779b51db2e97340112e88d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/app-on-react/precache-manifest.02e218c1f0c4008c80e730560afb4b48.js"
+  "/app-on-react/precache-manifest.0d90f3184a9e5f94cc00da2aca858033.js"
 );
 
 self.addEventListener('message', (event) => {
